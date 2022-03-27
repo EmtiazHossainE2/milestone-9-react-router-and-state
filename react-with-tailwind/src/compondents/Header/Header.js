d9e80d628ad11107ef2e5e38fd0a71e415d012c3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-27 23:57:23 +0600

    card tailwind css

--- react-with-tailwind/src/compondents/Header/Header.js
+++ react-with-tailwind/src/compondents/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import Pricing from '../Pricing/Pricing';
 
 const Header = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Pricing></Pricing>
         </div>
     );
 };
---
+++ react-with-tailwind/src/compondents/Pricing/Pricing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <div className='text-justify  mx-20 md:mx-10 sm:mx-5'>
+            <div className="text-5xl text-center mt-5">Today's best deals</div>
+            <p className='mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas itaque officiis esse doloribus possimus quisquam dolores reiciendis laboriosam iusto repudiandae magnam, perspiciatis distinctio aliquam incidunt consectetur quas eum dolorum et dolorem numquam deleniti excepturi vero? Cum officiis accusantium, sed magnam non unde fuga. Laboriosam excepturi, quam tenetur assumenda suscipit incidunt sequi nostrum quisquam magni fuga tempore reprehenderit id at molestias amet deleniti, voluptatibus, commodi vero? Numquam libero natus incidunt ab similique, ullam id, officia amet, beatae voluptatem eos quaerat quibusdam? Consequatur eius quod numquam laborum totam dolor! Earum harum libero minima, porro tempore facere assumenda labore cum itaque facilis.</p>
+        </div>
+    );
+};
+
+export default Pricing;
