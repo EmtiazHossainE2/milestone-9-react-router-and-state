351d93ec4b06d9c13878d67d17c8edac42f436e9
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-27 23:28:45 +0600

    xicon added

--- react-with-tailwind/src/compondents/Navbar/Navbar.js
+++ react-with-tailwind/src/compondents/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, name: 'Home', link: './home' },
         { id: 2, name: 'Shop', link: './shop' },
@@ -10,7 +12,10 @@ const Navbar = () => {
     ]
     return (
         <nav>
-            <ul className='md:flex justify-center mt-8'>
+            <div onClick={() => setOpen(!open)} className="h-8 w-8 md:hidden text-blue-500" >
+                {open ? <XIcon></XIcon> : <MenuIcon ></MenuIcon>}
+            </div>
+            <ul className={`md:flex justify-center absolute ease-in duration-700 ${open ? 'top-10' : 'top-[-300px]'}`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
