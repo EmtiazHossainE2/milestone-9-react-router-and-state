8d86c1808b59173910420fe9421489d437488a18
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-29 00:35:18 +0600

    create components

---
+++ react-router/src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is about us section</h2>
+            <p>Know about us .Send money </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ react-router/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is homepage </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, repellendus! Est fuga, obcaecati numquam beatae dolorem ullam debitis corrupti molestiae ipsam vero nostrum? Consequuntur nobis soluta pariatur iste ducimus quae, provident quisquam qui, nemo animi fuga dolores dolor sequi perferendis, quas delectus culpa expedita eligendi in maiores. Aliquid, necessitatibus quo?</p>
+        </div>
+    );
+};
+
+export default Home;
