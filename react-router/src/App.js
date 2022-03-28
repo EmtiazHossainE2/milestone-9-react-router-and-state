49038ddfbe1d7a25c0b947efc9fd273a2252a2c1
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-29 00:55:35 +0600

    simple routeing

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 
 const App = () => {
     return (
         <div>
-            <Home></Home>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ad quam sed pariatur quia sint in quisquam maiores, fugit voluptatibus expedita. Asperiores, laudantium illum corrupti est ab voluptates quod deserunt.
+            <Routes>
+                <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/about' element={<About></About>}></Route>
+                <Route path='contact' element={<Contact></Contact>}></Route>
+            </Routes>
         </div>
     );
 };
